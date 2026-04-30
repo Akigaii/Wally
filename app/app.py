64e31cee2b3d8281313a9c266d44ac3ed817b25b
Akigaii <reece.galgana@gmail.com>
2026-04-30 14:56:57 -0700

    changed app.py port configuration

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import json
+import os
 import random
 from flask import Flask, render_template, jsonify, request
 from deploy import predict
@@ -34,4 +35,5 @@ def predict_route():
     return jsonify({"score": round(score, 1)})
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
